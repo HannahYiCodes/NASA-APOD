4070ae367b40a0c071ca07391eba63ad40c2f511
HannahYiCodes <hannahyi.students@careerdev.com>
2022-08-12 20:03:28 -0400

    "APOD"

--- src/main/java/com/nasa/APOD/Controller/NasaController.java
+++ src/main/java/com/nasa/APOD/Controller/NasaController.java
@@ -1,12 +1,17 @@
 package com.nasa.APOD.Controller;
 
+        import com.fasterxml.jackson.databind.util.JSONPObject;
         import org.springframework.web.bind.annotation.*;
         import org.springframework.web.client.RestTemplate;
 
+        import java.awt.*;
+        import javax.swing.JFrame;
+
+        import java.awt.*;
         import java.util.Date;
 
 @RestController
-@RequestMapping("/Nasa")
+@RequestMapping("/nasa")
 public class NasaController {
     private String myNasaKey = "gu6DWzRFvzktFdRu2gN98jMxu6oJBlrZJfcodsDV";
     private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
@@ -20,6 +25,15 @@ public class NasaController {
     public Object apodRoute(RestTemplate restTemplate) {
         return restTemplate.getForObject(nasaApodEndpoint, Object.class);
     }
+
+//    public class MyCanvas extends Canvas{
+//        public void paint(Graphics g) {
+//            Toolkit t = Toolkit.getDefaultToolkit();
+//            Image i = t.getImage("image.png");
+//            g.drawImage(i, 120,100,this);
+//        }
+//    }
+
     //date
     //localhost:5000/v1/apod?api_key=DEMO_KEY&date=2014-10-01
 }
